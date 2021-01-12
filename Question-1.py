43c6d53bcd0a41db8216d111171f0e1bd7148de3
Abhishek854814 <71182161+Abhishek854814@users.noreply.github.com>
2021-01-12 22:19:51 +0530

    Update Question-1.py

--- Question-1.py
+++ Question-1.py
@@ -1,1 +1,10 @@
-gfyhtngvhnbm
+list1 = [5,10,25,18]
+   def sumfunction(list, size):          #sum function for sum of list
+      if(size==0):
+          return 0
+      else:
+          return list[size-1]+sum(list, size-1)
+
+    result = sumfunction(list, len(list1))
+    print("Sum of given list is: ", total) 
+

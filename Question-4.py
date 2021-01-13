09e36e12fa0ead6460056d835151973b06829fd7
Abhishek854814 <71182161+Abhishek854814@users.noreply.github.com>
2021-01-13 10:35:13 +0530

    Update Question-4.py

--- Question-4.py
+++ Question-4.py
@@ -1,24 +1,33 @@
+        import sqlite3
+         connection = sqlite3.connect("myTable.db)
+         crsr = connection.cursor()
+                                      
          #User Table will be:-
           
-          CREATE TABLE Users (
+         sql_command = """CREATE TABLE Users (
           Username   varchar(200) Foreign_key,
           Password   varchar(8)
-          );
+          );"""
+          crsr.execute(sql_command)                          
 
           #Address Table will be:-
 
-          CREATE TABLE Adresses(
+          sql_command = """CREATE TABLE Adresses(
           Username   varchar(200)
           Column_street  varchar(50),
           Pincode        int(6),
           Country        varchar(20),
           Phone          bigint(10)
-          );
-
+          );"""
+          crsr.execute(sql_command) 
+           connection.commit()                           
+                            
          #Querry will be:-
-          
-           SELECT Users.Username,
-           Adresses.Username,
-            FROM Users
-           INNER JOIN Adresses ON
-           Users.Username=Addresses.Username;
+                                      
+          crsr.execute("SELECT Users.Username,Adresses.Username,FROM Users INNER JOIN Adresses ON Users.Username=Addresses.Username;")
+            ans = crsr.fetchball()
+             print(ans)                         
+            
+           
+           
+           
